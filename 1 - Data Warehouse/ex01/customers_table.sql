4d646ed0ce2ba1e1ca0662d0822c27a7fe9ca7b1
jdomingu98 <jadominguezarcas@gmail.com>
2025-04-18 19:47:03 +0200

    feat: join all customers tables into a generic oneone
    customers tables are those from csv, one per each file

---
+++ 1 - Data Warehouse/ex01/customers_table.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS customers (
+	event_time TIMESTAMPTZ NOT NULL,
+	event_type TEXT NOT NULL,
+	product_id BIGINT NOT NULL,
+	price NUMERIC(10,2) NOT NULL,
+	user_id BIGINT NOT NULL,
+	user_session UUID
+);
+
+DO $$
+DECLARE
+    tab text;
+BEGIN
+    FOR tab IN 
+        SELECT tablename
+        FROM pg_tables
+        WHERE schemaname = 'public' AND tablename LIKE 'data_202_\_%' ESCAPE '\'
+    LOOP
+        EXECUTE format('
+            INSERT INTO customers
+            SELECT * FROM %I
+        ', tab);
+    END LOOP;
+END $$;
