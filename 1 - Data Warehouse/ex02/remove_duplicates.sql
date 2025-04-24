5dc6245d8041253388f7dbe7662f75c227d5913f
jdomingu98 <jadominguezarcas@gmail.com>
2025-04-24 02:01:01 +0200

    refactor: simplify query to delete exact duplicates

--- 1 - Data Warehouse/ex02/remove_duplicates.sql
+++ 1 - Data Warehouse/ex02/remove_duplicates.sql
@@ -1,18 +1,9 @@
 
---Exact same data
-DELETE FROM customers t1 USING (
-	SELECT MAX(ctid) as keep_ctid, *
-	FROM customers
-	GROUP BY event_time, event_type, product_id, price, user_id, user_session
-	HAVING COUNT(*) > 1
-) t2
-	WHERE t1.event_time = t2.event_time
-		AND t1.event_type = t2.event_type
-		AND t1.product_id = t2.product_id
-		AND t1.price = t2.price
-		AND t1.user_id = t2.user_id
-		AND t1.user_session = t2.user_session
-		AND t1.ctid <> t2.keep_ctid;
+-- Exact same data
+CREATE TEMPORARY TABLE tmp_customers AS
+	SELECT DISTINCT * FROM customers;
+TRUNCATE customers;
+INSERT INTO customers SELECT * FROM tmp_customers;
 
 -- Exact same data but with a event_time difference of 1 second or less
 WITH ordered_events AS (
